4302688fe8113b53c97c974a546e51da7db1beac
Gustavo <gus.inacio@hotmail.com>
2022-05-30 10:22:51 -0300

    feat: Socket events creation

--- src/webSocket.ts
+++ src/webSocket.ts
@@ -3,6 +3,15 @@ import { io } from "./http";
 io.on("connection", socket => {
   console.log(`user with id: ${socket.id} is online`);
 
+  socket.on("join_room", (data) => {
+    socket.join(data);
+    console.log("user joined room");
+  });
+
+  socket.on("send_message", (data) => {
+    console.log(data);
+  })
+
   socket.on("disconnect", () => {
     console.log(`user is now disconnected`);
   })
