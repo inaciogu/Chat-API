65d6d2914d18d1c928d1a30fa6db5bd6cc112167
Gustavo <gus.inacio@hotmail.com>
2022-05-30 19:30:38 -0300

    feat: Put whole logic in the same file

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,45 @@
-import { serverHttp } from './http';
-import "./webSocket"
+import express from 'express';
+import http from 'http';
+import cors from 'cors';
+import { Server } from 'socket.io';
+
+interface IMessage {
+  id: number;
+  room: string;
+  author: string;
+  time: string;
+  message: string;
+}
+
+const app = express();
+app.use(cors());
+
+const serverHttp = http.createServer(app);
+
+const io = new Server(serverHttp, {
+  cors: {
+    origin: "http://192.168.0.103:3000",
+    methods: ["POST", "POST"],
+  }
+});
+
+io.on("connection", socket => {
+  console.log(`user with id: ${socket.id} is online`);
+
+  socket.on("join_room", (data) => {
+    socket.join(data);
+    console.log("user joined room");
+  });
+
+  socket.on("send_message", (data: IMessage) => {
+    console.log(data);
+    socket.to(data.room).emit("receive_message", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log('user disconnected', socket.id);
+  });
+});
+
 
 serverHttp.listen(3001, () => console.log('Server is running on PORT 3001'))
