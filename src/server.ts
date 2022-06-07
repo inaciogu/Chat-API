9021f07e3007b33b49678df089f2befa2848ad8d
Gustavo <gus.inacio@hotmail.com>
2022-06-07 13:56:50 -0300

    fix: Adjusts the cors origin url

--- src/server.ts
+++ src/server.ts
@@ -20,7 +20,7 @@ const serverHttp = http.createServer(app);
 
 const io = new Server(serverHttp, {
   cors: {
-    origin: "https://chat-app-inaciogu.vercel.app/",
+    origin: "https://chat-app-inaciogu.vercel.app",
     methods: ["GET", "POST"],
   },
   transports: ["websocket", "polling"],
