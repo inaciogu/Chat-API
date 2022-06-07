9fc9902ff9614abc70b6375c37439dc6338e7adb
Gustavo <gus.inacio@hotmail.com>
2022-06-07 15:54:29 -0300

    fix: Adds another origin url to cors

--- src/server.ts
+++ src/server.ts
@@ -20,7 +20,7 @@ const serverHttp = http.createServer(app);
 
 const io = new Server(serverHttp, {
   cors: {
-    origin: "https://chat-app-inaciogu.vercel.app",
+    origin: ["https://chat-app-inaciogu.vercel.app", "http://localhost:3000"],
     methods: ["GET", "POST"],
   },
   transports: ["websocket", "polling"],
