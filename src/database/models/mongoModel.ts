841ea29d66ef14612aaad7288349164ef75cffff
Gustavo <gus.inacio@hotmail.com>
2022-06-21 14:15:49 -0300

    feat: Mongo abstract model

---
+++ src/database/models/mongoModel.ts
@@ -0,0 +1,15 @@
+import { Model as M, Document } from "mongoose";
+import IModel from "../interfaces/model";
+
+abstract class MongoModel<T> implements IModel<T> {
+  constructor(protected model: M<T & Document>) {}
+
+  create = async (obj: T): Promise<T> => this.model.create({ ...obj });
+
+  read = async (): Promise<T[]> => this.model.find();
+
+  readOne = async (id: string): Promise<T | null> =>
+    this.model.findOne({ _id: id });
+}
+
+export default MongoModel;
