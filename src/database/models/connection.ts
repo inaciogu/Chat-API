08961ec1c1c37420662e1465a33cea6d888b16c6
Gustavo <gus.inacio@hotmail.com>
2022-06-22 20:10:30 -0300

    perf: set the local mongo uri to a variable

--- src/database/models/connection.ts
+++ src/database/models/connection.ts
@@ -1,7 +1,9 @@
 import { connect } from "mongoose";
 import 'dotenv/config';
 
-const MONGO_URI = process.env.MONGO_DB_URI || 'mongodb://localhost:27017/RTC'
+const LOCAL_URI = 'mongodb://localhost:27017/RTC'
+
+const MONGO_URI = process.env.MONGO_DB_URI || LOCAL_URI
 
 const connectToDatabase = () => connect(MONGO_URI);
 
