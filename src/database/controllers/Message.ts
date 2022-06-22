20d079ff9d66b5b0e00174b21777b14716146459
Gustavo <gus.inacio@hotmail.com>
2022-06-22 10:05:30 -0300

    feat: Message controller errors handling

--- src/database/controllers/Message.ts
+++ src/database/controllers/Message.ts
@@ -2,19 +2,39 @@ import { Request, Response } from "express";
 import MessageService from "../services/Message";
 
 export default class MessageController {
-  constructor(protected service = new MessageService()) { }
+  constructor(protected service = new MessageService()) {}
 
   roomsMessages = async (req: Request, res: Response) => {
-    const { id }  = req.params;
+    try {
+      const { id } = req.params;
 
-    const response = await this.service.roomsMessages(id);
-    return res.status(200).json(response);
-  }
+      const response = await this.service.roomsMessages(id);
+      return response
+        ? res.status(200).json(response)
+        : res.status(404).json({
+            message: "Room not found",
+          });
+    } catch (error) {
+      return res.status(500).json({ message: "Internal server error" });
+    }
+  };
 
   newMessage = async (req: Request, res: Response) => {
-    const message = req.body;
+    try {
+      const message = req.body;
 
-    const response = await this.service.newMessage(message);
-    return res.status(201).json(response);
-  }
+      const response = await this.service.newMessage(message);
+
+      if (!response) {
+        return res.status(500).json({ message: "Internal server error" });
+      }
+
+      if ('error' in response) {
+        return res.status(400).json(response);
+      }
+      return res.status(201).json(response);
+    } catch (error) {
+      return res.status(500).json({ message: "Internal server error" });
+    }
+  };
 }
