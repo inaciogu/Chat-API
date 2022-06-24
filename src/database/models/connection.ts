6f90fbda4d2689b4f28af3db03fa592db1fe4b11
Gustavo <gus.inacio@hotmail.com>
2022-06-24 08:43:59 -0300

    fix: mongodb enviroment

--- src/database/models/connection.ts
+++ src/database/models/connection.ts
@@ -1,9 +1,7 @@
 import { connect } from "mongoose";
 import 'dotenv/config';
 
-const LOCAL_URI = 'mongodb://localhost:27017/RTC'
-
-const MONGO_URI = process.env.MONGO_DB_URI || LOCAL_URI
+const MONGO_URI = process.env.MONGO_DB_URI || '';
 
 const connectToDatabase = () => connect(MONGO_URI);
 
