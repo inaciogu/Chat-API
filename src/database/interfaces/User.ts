257f8b368fb1e7429b270c349a21cce449c19cbc
Gustavo <gus.inacio@hotmail.com>
2022-06-27 13:46:36 -0300

    feat: User model

--- src/database/interfaces/User.ts
+++ src/database/interfaces/User.ts
@@ -2,6 +2,7 @@ import { z } from "zod";
 
 export const UserSchema = z.object({
   name: z.string().min(3),
+  username: z.string().min(3),
   email: z.string().email(),
   password: z.string().min(5)
 });
---
+++ src/database/models/User.ts
@@ -0,0 +1,20 @@
+import { Document, model as createModel, Schema } from "mongoose";
+import { User } from "../interfaces/User";
+import MongoModel from './MongoModel';
+
+interface UserDocument extends User, Document {}
+
+const userSchema = new Schema<UserDocument>({
+  name: { type: String, required: true },
+  username: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true }
+}, {
+  versionKey: false
+});
+
+export default class UserModel extends MongoModel<User> {
+  constructor(model = createModel('user', userSchema)) {
+    super(model);
+  }
+}
